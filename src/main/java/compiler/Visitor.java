1375bb325b33e4b50eb5299407b9efbf73942218
fh4417 <fh4417@ic.ac.uk>
2019-02-05 14:56:38 +0000

    add tests for invalid programs

--- src/main/java/compiler/Visitor.java
+++ src/main/java/compiler/Visitor.java
@@ -21,22 +21,61 @@ public class Visitor extends BasicParserBaseVisitor<Void>{
   }
 
   @Override
-  public  Void visitExpr(BasicParser.ExprContext ctx) {
+  public Void visitExpr(BasicParser.ExprContext ctx) {
 
 
     if(ctx.INTEGER() != null){
 
-      String value = ctx.getText();
-
-      try {
-        Integer.parseInt(value);
-      } catch (NumberFormatException e) {
-        parser.notifyErrorListeners("Badly formated Integer");
-      }
-
+      //CheckInteger(ctx.getText());
     }
-    
+
+    /* if(ctx.bool_liter() != null){
+    }
+
+    if(ctx.char_liter() != null){
+    }
+
+    if(ctx.str_liter() != null){
+    }
+
+    if(ctx.pair_liter() != null){
+    }
+
+    if(ctx.IDENT() != null) {
+    }
+
+    if(ctx.array_elem() != null) {
+    }
+
+    if(ctx.unary_oper() != null){
+    }
+
+    if (ctx.binary_oper() != null){
+    }
+
+    if(ctx.OPEN() != null){
+    }
+
+    if(ctx.CLOSE() != null){
+    }*/
+
     return null;
+  }
+
+  @Override
+  public Void visitBinary_oper(BasicParser.Binary_operContext ctx){
+
+    return null;
+  }
+
+  public void CheckInteger(String value){
+
+    try {
+      Integer.parseInt(value);
+    } catch (NumberFormatException e) {
+      parser.notifyErrorListeners("Badly formated Integer");
+    }
+
   }
 }
 
