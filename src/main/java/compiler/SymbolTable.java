93c96e34433e44b7c14bf0a432dfd160340cd048
Theo Cohen <theo.cohen17@imperial.ac.uk>
2019-02-06 15:11:29 +0000

    [gtb17, tsc17] tdd cycle for basic symbol table implementation

---
+++ src/main/java/compiler/SymbolTable.java
@@ -0,0 +1,38 @@
+package compiler;
+
+import java.util.HashMap;
+
+public class SymbolTable {
+
+  SymbolTable encSymTable;
+  HashMap<String, Identifier> dict;
+
+  public SymbolTable(SymbolTable encSymTable) {
+    this.encSymTable = encSymTable;
+    dict = new HashMap<>();
+  }
+
+  public void add(String name, Identifier ident) {
+    dict.put(name, ident);
+  }
+
+  public Identifier lookUpAll(String name) {
+    SymbolTable currST = this;
+    while (currST != null) {
+      Identifier ident = currST.lookUpScope(name);
+      if (ident != null) {
+        return ident;
+      }
+      currST = currST.getEncSymTable();
+    }
+    return null;
+  }
+
+  public Identifier lookUpScope(String name) {
+    return this.dict.get(name);
+  }
+
+  public SymbolTable getEncSymTable() {
+    return encSymTable;
+  }
+}
---
+++ src/test/java/SymbolTableUnitTests.java
@@ -0,0 +1,32 @@
+import compiler.Identifier;
+import compiler.SymbolTable;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+public class SymbolTableUnitTests {
+
+  @Rule
+  public JUnitRuleMockery context = new JUnitRuleMockery();
+
+  Identifier identifier = context.mock(Identifier.class);
+  SymbolTable st = new SymbolTable(null);
+
+
+  @Test
+  public void canLookUpInCurrentSymbolTable() {
+    st.add("x", identifier);
+    assertThat(st.lookUpScope("x"), is(notNullValue()));
+  }
+
+  @Test
+  public void canLookUpInEnclosingSymbolTable() {
+    st.add("x", identifier);
+    SymbolTable stChild = new SymbolTable(st);
+    assertThat(stChild.lookUpAll("x"), is(notNullValue()));
+  }
+
+}
