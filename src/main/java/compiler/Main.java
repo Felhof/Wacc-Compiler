805dac6fa6aa9c00eca0d2d1b9ec24c4b5992e1f
Gauthier  Bonvarlet <gauthier.bonvarlet17@imperial.ac.uk>
2019-02-11 18:44:50 +0000

    Read statement can now take in char types, fix binary operations to include pairs and arrays

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
   public static void main(String[] args) {
     //ASTNode ast = compileProg(args[0]); // uncomment for labTS test
-    ASTNode ast = compileProg("src/test/valid/function/simple_functions/functionReturnPair.wacc");
+    ASTNode ast = compileProg("src/test/valid/pairs/checkRefPair.wacc");
     System.out.println(ast.toString());
     System.exit(0);
   }
