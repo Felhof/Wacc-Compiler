f0cc6627d03aea65f4f380e51ada5392afdde880
fh4417 <fh4417@ic.ac.uk>
2019-02-19 14:19:21 +0000

    Check printed output of assembled program

--- src/test/java/codegen/CodegenTests.java
+++ src/test/java/codegen/CodegenTests.java
@@ -39,20 +39,58 @@ public class CodegenTests {
 
         assertThat(emulator.exitValue(), is(expectedExitCode[i]));
 
-//        //This might be usefull when we want the output:
-//
-//        BufferedReader br=new BufferedReader(new InputStreamReader(emulator
-//        .getInputStream()));
-//        String line;
-//        StringBuilder sb = new StringBuilder();
-//        while((line=br.readLine())!=null) sb.append(line);
-//
-//        System.out.println(sb.toString());
+        //This might be usefull when we want the output:
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(emulator
+        .getInputStream()));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while((line=br.readLine())!=null) sb.append(line);
+
+        System.out.println("Output " + i + ": " + sb.toString());
 
       } catch (IOException | InterruptedException e) {
         e.printStackTrace();
       }
     }
+  }
+
+  //Example of how we can test the printed output of a program
+  String filename = "printTwoLines";
+  String[] expectedLines = new String[]{"True is true", "False is false"};
+
+  @Test
+  public void OutputTest(){
+
+    try {
+
+      // Assembler
+      Process assembler = new ProcessBuilder("arm-linux-gnueabi-gcc", "-o",
+              filename, "-mcpu=arm1176jzf-s", "-mtune=arm1176jzf-s",
+              filename + ".s").start();
+      assembler.waitFor();
+
+      // Emulator
+      Process emulator = new ProcessBuilder("qemu-arm", "-L", "/usr"
+              + "/arm-linux-gnueabi/", filename).start();
+      emulator.waitFor();
+
+      //Read each line of the output into the sb
+      BufferedReader br=new BufferedReader(new InputStreamReader(emulator
+              .getInputStream()));
+
+      //StringBuilder sb = new StringBuilder();
+      for(String expectedLine : expectedLines){
+        String actualLine = br.readLine();
+        assertThat(actualLine, is(expectedLine));
+      }
+
+
+
+    } catch (IOException | InterruptedException e) {
+      e.printStackTrace();
+    }
+
   }
 
 }
