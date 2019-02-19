11b6f3a8cd6e8c4153a6e3203c478dab3e4bb316
fh4417 <fh4417@ic.ac.uk>
2019-02-19 15:08:09 +0000

    sequenced containing skip and exit work

--- src/test/java/codegen/CodegenTests.java
+++ src/test/java/codegen/CodegenTests.java
@@ -5,13 +5,14 @@ import compiler.Main;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class CodegenTests {
-
-
+  
   private Process assembleAndEmulate (String filename) {
 
       try {
@@ -41,43 +42,77 @@ public class CodegenTests {
   public void ExitCodeTest(){
 
     String path = "src/test/examples/valid/basic/exit/";
-    String[] fileNames = { "exit-1","exitBasic", "exitBasic2", "exitWrap"};
+    String[] filenames = { "exit-1","exitBasic", "exitBasic2", "exitWrap"};
     int[] expectedExitCodes = { 255, 7, 42, 0};
 
 
-    for (int i = 0; i < fileNames.length; i++) {
-      String filename = fileNames[i];
+    IntStream.range(0, filenames.length).forEach(i -> {
+      String filename = filenames[i];
       AST ast = Main.compileProg(path + filename + ".wacc");
       Main.generateCode(ast, filename);
-
       Process emulator = assembleAndEmulate(filename);
-
       assertThat(emulator.exitValue(), is(expectedExitCodes[i]));
-    }
+    });
   }
 
   @Test
   public void SkipTest(){
     String path = "src/test/examples/valid/basic/skip/";
-    String[] fileNames = {"skip", "comment","skip"};
+    String[] filenames = {"skip", "comment","skip"};
 
-    for (int i = 0; i < fileNames.length; i++) {
-      String filename = fileNames[i];
+    Arrays.stream(filenames).forEach(filename -> {
       AST ast = Main.compileProg(path + filename + ".wacc");
       Main.generateCode(ast, filename);
-
       Process emulator = assembleAndEmulate(filename);
-
       assertThat(emulator.exitValue(), is(0));
+    });
+  }
+
+  @Test
+  public void SequenceTest(){
+    String path = "src/test/examples/valid/sequence/";
+    String[] simpleFilenames = {"basicSeq", "basicSeq2"};
+
+    //Simply test that they exit correctly
+    Arrays.stream(simpleFilenames).forEach(filename -> {
+      AST ast = Main.compileProg(path + filename + ".wacc");
+      Main.generateCode(ast, filename);
+      Process emulator = assembleAndEmulate(filename);
+      assertThat(emulator.exitValue(), is(0));
+    });
+
+    String[] filenames = {"exitSimple"};
+    String[][] expectedOutput = {{null}}; //exitSimple is not supposed to print anything
+
+    //Test that their output is correct
+
+    for(int i = 0; i < filenames.length; i++){
+
+      Process emulator = assembleAndEmulate(filenames[i]);
+
+      try {
+
+        //Read each line of the output into the sb
+        BufferedReader br = new BufferedReader(new InputStreamReader(emulator
+                .getInputStream()));
+
+        //StringBuilder sb = new StringBuilder();
+        for (String expectedLine : expectedOutput[i]) {
+          String actualLine = br.readLine();
+          assertThat(actualLine, is(expectedLine));
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 
   //Example of how we can test the printed output of a program
-  String filename = "printTwoLines";
-  String[] expectedLines = new String[]{"True is true", "False is false"};
-
   @Test
   public void OutputTest(){
+
+    String filename = "printTwoLines";
+    String[] expectedLines = new String[]{"True is true", "False is false"};
 
     try {
 
@@ -92,8 +127,6 @@ public class CodegenTests {
         String actualLine = br.readLine();
         assertThat(actualLine, is(expectedLine));
       }
-
-
 
     } catch (IOException e) {
       e.printStackTrace();
