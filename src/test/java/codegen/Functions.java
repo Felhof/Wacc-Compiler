e7ee00a89f9639016a50da4a1365be50f7252ced
Gauthier  Bonvarlet <gauthier.bonvarlet17@imperial.ac.uk>
2019-02-24 20:19:02 +0000

    Add further tests for simple functions

--- src/test/java/codegen/Functions.java
+++ src/test/java/codegen/Functions.java
@@ -10,10 +10,11 @@ public class Functions {
   public void simpleFunctions() {
     String path = "src/test/examples/valid/function/simple_functions/";
     String[] filenames = {"functionDeclaration", "functionSimple",
-        "sameArgName", "sameArgName2", "negFunction", "incFunction"}; //};,
+        "sameArgName", "sameArgName2", "negFunction", "incFunction", "functionReturnPair", "functionUpdateParameter"}; //};,
         //"functionManyArguments"};
     String[][] outputs = {{}, {}, {"99"}, {"99"}, {"true", "false", "true"},
-        {"1", "4"}};/* {"a is 42",
+        {"1", "4"}, {"10"}, {"y is 1", "x is 1", "x is now 5", "y is still 1"}};
+        /* {"a is 42",
         "b is true", "c is u", "d is hello", "e is 0x22008", "f is 0x22018",
         "answer is g"}}; */
     compileAndCheckExitAndOutput(path, filenames, null, null, outputs);
