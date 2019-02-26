e915a9a412fd2347d583e60826d9ee9aeb74330c
Theo Cohen <theo.cohen17@imperial.ac.uk>
2019-02-26 23:25:06 +0000

    Removed brackets concept as it is already taken into account in parsing

--- src/main/java/compiler/AST/NodeElements/RHS/Expr.java
+++ src/main/java/compiler/AST/NodeElements/RHS/Expr.java
@@ -5,14 +5,8 @@ import compiler.AST.Types.Type;
 import compiler.instr.Operand.Operand;
 
 public abstract class Expr extends NodeElem {
-  protected boolean hasBrackets = false;
-
   public Expr(Type type) {
     super(type);
-  }
-
-  public void putBrackets() {
-    this.hasBrackets = true;
   }
 
   public abstract int sizeOf();
